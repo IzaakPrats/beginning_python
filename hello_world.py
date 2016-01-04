205f8173dae4868c81f11b3e1f49a35886f4d99d
Izaak Prats <iprats@interapt.com>
2016-01-03 22:29:54 -0500

    User input is cool. So is string concatenation!

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,7 @@ f = 3.1415926
 name = "Python"
 big = 234987456
 z = complex(2, 3)
+
 print(x)
 print(f)
 print(name)
---
+++ user_input.py
@@ -0,0 +1,5 @@
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+sum = x + y
+print(str(x) + ' + ' + str(y) + ' = ' + str(sum))
